6b3523800ef7cf02d6ca42f38c9ccec7161fc3ad
Daniel Cervantes <robous@gmail.com>
2018-07-06 23:49:34 -0600

    rules for nginx install

--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
-LatestVirtualBoxVersion=$(wget -qO - http://download.virtualbox.org/virtualbox/LATEST.TXT) && wget "http://download.virtualbox.org/virtualbox/${LatestVirtualBoxVersion}/Oracle_VM_VirtualBox_Extension_Pack-${LatestVirtualBoxVersion}.vbox-extpack"
+#LatestVirtualBoxVersion=$(wget -qO - http://download.virtualbox.org/virtualbox/LATEST.TXT) && wget "http://download.virtualbox.org/virtualbox/${LatestVirtualBoxVersion}/Oracle_VM_VirtualBox_Extension_Pack-${LatestVirtualBoxVersion}.vbox-extpack"
+#VBoxManage extpack install --replace Oracle_VM_VirtualBox_Extension_Pack-${LatestVirtualBoxVersion}.vbox-extpack
 
-sudo VBoxManage extpack install --replace Oracle_VM_VirtualBox_Extension_Pack-${LatestVirtualBoxVersion}.vbox-extpack
+
+##
+# Install nginx
+##
+wget -q http://nginx.org/keys/nginx_signing.key -O- | apt-key add -
+add-apt-repository 'deb http://nginx.org/packages/mainline/ubuntu/ bionic nginx'
+add-apt-repository 'deb-src http://nginx.org/packages/mainline/ubuntu/ bionic nginx'
+apt update
+apt install nginx
+apt install nginx-module-njs -y
+rm /etc/nginx/nginx.conf
+cp nginx/nginx.conf /etc/nginx/
